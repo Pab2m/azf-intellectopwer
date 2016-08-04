ae5a2ddfca501c2ce6b431e4c74d0edb02a64ab4
Pab2m <Za.ra.thus.tra@yandex.ru>
2016-08-04 17:26:34 +0500

    hover

--- html/js/main.js
+++ html/js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
 
     $("#email-button").add("#entry").hover(function(){
-      $(this).find(".glyphicon").css({color:"#108F91"});
+    $(this).find(".glyphicon").css({color:"#108F91",
+    transition:"all 0.6s ease 0s"});
     },
     function(){
-      $(this).find(".glyphicon").css({color:"#CCC"});
+      $(this).find(".glyphicon").css({color:"#CCC",
+      transition:"all 0.6s ease 0s"});
     });
      $("#input-search").focus(function(){
        $("#search").css({border:"1px solid #60ADE4"});
